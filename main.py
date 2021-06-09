aa849b0f846b64a14ba8e631bbfc1103a8465c72
Simonsays <simonsaysfollow@gmail.com>
2021-06-09 07:43:27 -0700

    testing api

--- main.py
+++ main.py
@@ -38,6 +38,21 @@ app = FastAPI()
 def welcome():
       return "Hello world"
 
+
+@app.post("api/get/all/")
+def get_gems_by_user(gem:Gems):
+    try:
+        array_of_user_gems = []
+        all_gems = main.database.child("GEMS").get(gem.token)
+        
+        if type(all_gems) != 'NoneType':
+            for x in all_gems.pyres:
+                # if x.item[1]["ownerID"] == gem.ownerID:
+                array_of_user_gems.append(x.item)
+            return array_of_user_gems
+    except:
+        return "Database is empty"
+
 # firebaseConfig = {
 #   "apiKey": settings.API_KEY,
 #   "authDomain": settings.AUTH_DOMAIN,

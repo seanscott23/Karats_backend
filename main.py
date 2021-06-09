6316acdb4f6887e9416fb7479010c845fae7f5ee
Simonsays <simonsaysfollow@gmail.com>
2021-06-09 10:31:45 -0700

    testing post

--- main.py
+++ main.py
@@ -74,8 +74,8 @@ def get_gems_by_user():
   try:
     array_of_user_gems = []
     print(database.child("GEMS"))
-    all_gems = database.child("GEMS").get()
-    print(all_gems.key)
+    all_gems = database.child("GEMS").get().val()
+    print(all_gems)
     for result in all_gems.each():
       print(result.val())
     if type(all_gems) != 'NoneType':

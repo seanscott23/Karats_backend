e1f815e76cb3cd566ca5ab9318328b14ecf899f4
Simonsays <simonsaysfollow@gmail.com>
2021-06-09 23:45:01 -0700

    testing

--- Reuse/audio.py
+++ Reuse/audio.py
@@ -22,7 +22,6 @@ def post_audio(audioMeta: Audio):
     print("here i am")
     print(audioMeta.url)
     audio = requests.get(audioMeta.url, timeout=10)
-    pr
     audioID = Helper.getAudioID(audio.url)
     original = AudioSegment.from_mp3(BytesIO(audio.content))
     begin = audioMeta.begin * 1000

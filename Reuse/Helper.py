2bd2475005af26a50d0ea5a8e77ae57d2c896c88
Simonsays <simonsaysfollow@gmail.com>
2021-06-10 17:00:02 -0700

    testing

--- Reuse/Helper.py
+++ Reuse/Helper.py
@@ -4,6 +4,6 @@ from pydantic import BaseModel
 
 class Helper():
     
-    def getAudioID(audioUrl):
+    def getAudioID(self, audioUrl):
         audioUrlList = audioUrl.split("/")
         return audioUrlList[3]

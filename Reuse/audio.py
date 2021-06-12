65812dc6440e5d47c0b21be01e8fc6cb9f61445f
Simonsays <simonsaysfollow@gmail.com>
2021-06-12 15:51:58 -0700

    testing

--- Reuse/audio.py
+++ Reuse/audio.py
@@ -19,18 +19,17 @@ class Audio(BaseModel):
 
 @router.post("/api/deliver/audio/")
 def post_audio(audioMeta: Audio):
-    print("this is the audiourl" + audioMeta.url)
-    # audio = requests.get(audioMeta.url, timeout=10)
-    # audioID = Helper.getAudioID(audio.url)
-    # original = AudioSegment.from_mp3(BytesIO(audio.content))
-    # begin = audioMeta.begin * 1000
-    # end = audioMeta.end * 1000
-    # section = original[begin:end]
-    # buf = BytesIO()
-    # section.export(buf, format="mp3")
-    # sendAudioToStorage(audioID, buf, audioMeta.userID, audioMeta.token)
-    # json_response = jsonable_encoder({"trimmed_audio_url": get_audio(audioMeta.userID, audioID, audioMeta.token)})
-    # return json_response
+    audio = requests.get(audioMeta.url, timeout=10)
+    audioID = Helper.getAudioID(audio.url)
+    original = AudioSegment.from_mp3(BytesIO(audio.content))
+    begin = audioMeta.begin * 1000
+    end = audioMeta.end * 1000
+    section = original[begin:end]
+    buf = BytesIO()
+    section.export(buf, format="mp3")
+    sendAudioToStorage(audioID, buf, audioMeta.userID, audioMeta.token)
+    json_response = jsonable_encoder({"trimmed_audio_url": get_audio(audioMeta.userID, audioID, audioMeta.token)})
+    return json_response
 
 def sendAudioToStorage(audioID, sectionOfAudio, userID, token):
     main.storage.child(userID).child(audioID).put(sectionOfAudio, token)
@@ -41,7 +40,17 @@ def sendAudioToStorage(audioID, sectionOfAudio, userID, token):
 def get_audio(userID, audioID, token):
     return main.storage.child(userID).child(audioID).get_url(token)
 
-# def getAudioID(audioUrl):
-#     print(audioUrl)
-#     audioUrlList = audioUrl.split("/")
-#     return audioUrlList[3]
+
+@router.post("/api/deliver/mp3/audio/")
+async def post_mp3_audio(file:UploadFile = Form(...), userID:str = Form(...), token:str = Form(...), begin:str = Form(...), end:str = Form(...) ):
+    token = token
+    userID = userID
+    begin = float(begin) * 1000
+    end = float(end) * 1000
+    file_from_client = file.file
+    original = AudioSegment.from_file(file_from_client)
+    section = original[begin:end]
+    buf = BytesIO()
+    section.export(buf, format="mp3")
+    sendAudioToStorage("43", buf, userID, token)
+    return {"trimmed_audio_url": get_audio(userID, "43", token)} 

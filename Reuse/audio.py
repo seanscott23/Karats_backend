7f1a545c10bb6536db2feed657039a2adf61b192
Simonsays <simonsaysfollow@gmail.com>
2021-06-12 15:39:25 -0700

    testing

--- Reuse/audio.py
+++ Reuse/audio.py
@@ -20,17 +20,17 @@ class Audio(BaseModel):
 @router.post("/api/deliver/audio/")
 def post_audio(audioMeta: Audio):
     print("this is the audiourl" + audioMeta.url)
-    audio = requests.get(audioMeta.url, timeout=10)
-    audioID = Helper.getAudioID(audio.url)
-    original = AudioSegment.from_mp3(BytesIO(audio.content))
-    begin = audioMeta.begin * 1000
-    end = audioMeta.end * 1000
-    section = original[begin:end]
-    buf = BytesIO()
-    section.export(buf, format="mp3")
-    sendAudioToStorage(audioID, buf, audioMeta.userID, audioMeta.token)
-    json_response = jsonable_encoder({"trimmed_audio_url": get_audio(audioMeta.userID, audioID, audioMeta.token)})
-    return json_response
+    # audio = requests.get(audioMeta.url, timeout=10)
+    # audioID = Helper.getAudioID(audio.url)
+    # original = AudioSegment.from_mp3(BytesIO(audio.content))
+    # begin = audioMeta.begin * 1000
+    # end = audioMeta.end * 1000
+    # section = original[begin:end]
+    # buf = BytesIO()
+    # section.export(buf, format="mp3")
+    # sendAudioToStorage(audioID, buf, audioMeta.userID, audioMeta.token)
+    # json_response = jsonable_encoder({"trimmed_audio_url": get_audio(audioMeta.userID, audioID, audioMeta.token)})
+    # return json_response
 
 def sendAudioToStorage(audioID, sectionOfAudio, userID, token):
     main.storage.child(userID).child(audioID).put(sectionOfAudio, token)
